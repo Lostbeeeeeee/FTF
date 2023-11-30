415e18a436753fe55486b3c854e995db04933d13
Lostbeeeeeee <ll150418@163.com>
2023-11-30 16:17:55 +0800

    提交文件

---
+++ dalib/adaptation/cdan.py
@@ -0,0 +1,282 @@
+from typing import Optional
+import torch.nn as nn
+import torch.nn.functional as F
+from common.modules.classifier import Classifier as ClassifierBase
+from common.utils.metric import binary_accuracy
+from ..modules.grl import WarmStartGradientReverseLayer
+from ..modules.entropy import entropy
+
+
+__all__ = ['ConditionalDomainAdversarialLoss', 'ImageClassifier']
+
+
+import torch
+import numpy as np
+
+###############################################################
+
+def extract_ampl_phase(fft_im):
+    # fft_im: size should be bx3xhxwx2
+    fft_amp = fft_im[:,:,:,:,0]**2 + fft_im[:,:,:,:,1]**2
+    fft_amp = torch.sqrt(fft_amp)
+    fft_pha = torch.atan2( fft_im[:,:,:,:,1], fft_im[:,:,:,:,0] )
+    return fft_amp, fft_pha
+
+def low_freq_mutate( amp_src, amp_trg, L=0.1 ):
+    _, _, h, w = amp_src.size()
+    b = (  np.floor(np.amin((h,w))*L)  ).astype(int)     # get b
+    amp_src[:,:,0:b,0:b]     = amp_trg[:,:,0:b,0:b]      # top left
+    amp_src[:,:,0:b,w-b:w]   = amp_trg[:,:,0:b,w-b:w]    # top right
+    amp_src[:,:,h-b:h,0:b]   = amp_trg[:,:,h-b:h,0:b]    # bottom left
+    amp_src[:,:,h-b:h,w-b:w] = amp_trg[:,:,h-b:h,w-b:w]  # bottom right
+    return amp_src
+
+def low_freq_average( amp_src, amp_trg, L=0.1 ):
+    _, _, h, w = amp_src.size()
+    b = (  np.floor(np.amin((h,w))*L)  ).astype(int)     # get b
+    amp_src[:,:,0:b,0:b]     =  (amp_src[:,:,0:b,0:b]+amp_trg[:,:,0:b,0:b])/2      # top left
+    amp_src[:,:,0:b,w-b:w]   = (amp_trg[:,:,0:b,w-b:w]+amp_src[:,:,0:b,w-b:w])/2    # top right
+    amp_src[:,:,h-b:h,0:b]   = (amp_src[:,:,h-b:h,0:b]+amp_trg[:,:,h-b:h,0:b])/2    # bottom left
+    amp_src[:,:,h-b:h,w-b:w] = (amp_trg[:,:,h-b:h,w-b:w]+amp_src[:,:,h-b:h,w-b:w])/2  # bottom right
+    return amp_src
+
+def low_freq_mutate_np( amp_src, amp_trg, L=0.1 ):
+    a_src = np.fft.fftshift( amp_src, axes=(-2, -1) )
+    a_trg = np.fft.fftshift( amp_trg, axes=(-2, -1) )
+
+    h, w = a_src.shape
+    b = (  np.floor(np.amin((h,w))*L)  ).astype(int)
+    c_h = np.floor(h/2.0).astype(int)
+    c_w = np.floor(w/2.0).astype(int)
+
+    h1 = c_h-b
+    h2 = c_h+b+1
+    w1 = c_w-b
+    w2 = c_w+b+1
+
+    a_src[h1:h2,w1:w2] = a_trg[h1:h2,w1:w2]
+    a_src = np.fft.ifftshift( a_src, axes=(-2, -1) )
+    return a_src
+
+def FDA_source_to_target(src_img, trg_img, L=0.1):
+    # exchange magnitude
+    # input: src_img, trg_img
+
+    # get fft of both source and target
+    fft_src = torch.rfft( src_img.clone(), signal_ndim=2, onesided=False )
+    fft_trg = torch.rfft( trg_img.clone(), signal_ndim=2, onesided=False )
+
+    # extract amplitude and phase of both ffts
+    amp_src, pha_src = extract_ampl_phase( fft_src.clone())
+    amp_trg, pha_trg = extract_ampl_phase( fft_trg.clone())
+
+    # replace the low frequency amplitude part of source with that from target
+    amp_src_ = low_freq_mutate( amp_src.clone(), amp_trg.clone(), L=L )
+
+    # recompose fft of source
+    fft_src_ = torch.zeros( fft_src.size(), dtype=torch.float )
+    fft_src_[:,:,:,:,0] = torch.cos(pha_src.clone()) * amp_src_.clone()
+    fft_src_[:,:,:,:,1] = torch.sin(pha_src.clone()) * amp_src_.clone()
+
+    # get the recomposed image: source content, target style
+    _, _, imgH, imgW = src_img.size()
+    src_in_trg = torch.irfft( fft_src_, signal_ndim=2, onesided=False, signal_sizes=[imgH,imgW] )
+
+    return src_in_trg
+
+def FDA_source_to_target_np( src_img, trg_img, L=0.1 ):
+    # exchange magnitude
+    # input: src_img, trg_img
+
+    src_img_np = src_img #.cpu().numpy()
+    trg_img_np = trg_img #.cpu().numpy()
+
+    # get fft of both source and target
+    fft_src_np = np.fft.fft2( src_img_np, axes=(-2, -1) )
+    fft_trg_np = np.fft.fft2( trg_img_np, axes=(-2, -1) )
+
+    # extract amplitude and phase of both ffts
+    amp_src, pha_src = np.abs(fft_src_np), np.angle(fft_src_np)
+    amp_trg, pha_trg = np.abs(fft_trg_np), np.angle(fft_trg_np)
+
+    # mutate the amplitude part of source with target
+    amp_src_ = low_freq_mutate_np( amp_src, amp_trg, L=L )
+
+    # mutated fft of source
+    fft_src_ = amp_src_ * np.exp( 1j * pha_src )
+
+    # get the mutated image
+    src_in_trg = np.fft.ifft2( fft_src_, axes=(-2, -1) )
+    src_in_trg = np.real(src_in_trg)
+
+    return src_in_trg
+
+###############################################################
+
+class ConditionalDomainAdversarialLoss(nn.Module):
+    r"""The Conditional Domain Adversarial Loss used in `Conditional Adversarial Domain Adaptation (NIPS 2018) <https://arxiv.org/abs/1705.10667>`_
+
+    Conditional Domain adversarial loss measures the domain discrepancy through training a domain discriminator in a
+    conditional manner. Given domain discriminator :math:`D`, feature representation :math:`f` and
+    classifier predictions :math:`g`, the definition of CDAN loss is
+
+    .. math::
+        loss(\mathcal{D}_s, \mathcal{D}_t) &= \mathbb{E}_{x_i^s \sim \mathcal{D}_s} \text{log}[D(T(f_i^s, g_i^s))] \\
+        &+ \mathbb{E}_{x_j^t \sim \mathcal{D}_t} \text{log}[1-D(T(f_j^t, g_j^t))],\\
+
+    where :math:`T` is a :class:`MultiLinearMap`  or :class:`RandomizedMultiLinearMap` which convert two tensors to a single tensor.
+
+    Args:
+        domain_discriminator (torch.nn.Module): A domain discriminator object, which predicts the domains of
+          features. Its input shape is (N, F) and output shape is (N, 1)
+        entropy_conditioning (bool, optional): If True, use entropy-aware weight to reweight each training example.
+          Default: False
+        randomized (bool, optional): If True, use `randomized multi linear map`. Else, use `multi linear map`.
+          Default: False
+        num_classes (int, optional): Number of classes. Default: -1
+        features_dim (int, optional): Dimension of input features. Default: -1
+        randomized_dim (int, optional): Dimension of features after randomized. Default: 1024
+        reduction (str, optional): Specifies the reduction to apply to the output:
+          ``'none'`` | ``'mean'`` | ``'sum'``. ``'none'``: no reduction will be applied,
+          ``'mean'``: the sum of the output will be divided by the number of
+          elements in the output, ``'sum'``: the output will be summed. Default: ``'mean'``
+
+    .. note::
+        You need to provide `num_classes`, `features_dim` and `randomized_dim` **only when** `randomized`
+        is set True.
+
+    Inputs:
+        - g_s (tensor): unnormalized classifier predictions on source domain, :math:`g^s`
+        - f_s (tensor): feature representations on source domain, :math:`f^s`
+        - g_t (tensor): unnormalized classifier predictions on target domain, :math:`g^t`
+        - f_t (tensor): feature representations on target domain, :math:`f^t`
+
+    Shape:
+        - g_s, g_t: :math:`(minibatch, C)` where C means the number of classes.
+        - f_s, f_t: :math:`(minibatch, F)` where F means the dimension of input features.
+        - Output: scalar by default. If :attr:`reduction` is ``'none'``, then :math:`(minibatch, )`.
+
+    Examples::
+
+        >>> from dalib.modules.domain_discriminator import DomainDiscriminator
+        >>> from dalib.adaptation.cdan import ConditionalDomainAdversarialLoss
+        >>> import torch
+        >>> num_classes = 2
+        >>> feature_dim = 1024
+        >>> batch_size = 10
+        >>> discriminator = DomainDiscriminator(in_feature=feature_dim * num_classes, hidden_size=1024)
+        >>> loss = ConditionalDomainAdversarialLoss(discriminator, reduction='mean')
+        >>> # features from source domain and target domain
+        >>> f_s, f_t = torch.randn(batch_size, feature_dim), torch.randn(batch_size, feature_dim)
+        >>> # logits output from source domain adn target domain
+        >>> g_s, g_t = torch.randn(batch_size, num_classes), torch.randn(batch_size, num_classes)
+        >>> output = loss(g_s, f_s, g_t, f_t)
+    """
+
+    def __init__(self, domain_discriminator: nn.Module, entropy_conditioning: Optional[bool] = False,
+                 randomized: Optional[bool] = False, num_classes: Optional[int] = -1,
+                 features_dim: Optional[int] = -1, randomized_dim: Optional[int] = 1024,
+                 reduction: Optional[str] = 'mean'):
+        super(ConditionalDomainAdversarialLoss, self).__init__()
+        self.domain_discriminator = domain_discriminator
+        self.grl = WarmStartGradientReverseLayer(alpha=1., lo=0., hi=1., max_iters=1000, auto_step=True)
+        self.entropy_conditioning = entropy_conditioning
+
+        if randomized:
+            assert num_classes > 0 and features_dim > 0 and randomized_dim > 0
+            self.map = RandomizedMultiLinearMap(features_dim, num_classes, randomized_dim)
+        else:
+            self.map = MultiLinearMap()
+
+        self.bce = lambda input, target, weight: F.binary_cross_entropy(input, target, weight,
+                                                                        reduction=reduction) if self.entropy_conditioning \
+            else F.binary_cross_entropy(input, target, reduction=reduction)
+        self.domain_discriminator_accuracy = None
+
+    def forward(self, g_s: torch.Tensor, f_s: torch.Tensor, g_t: torch.Tensor, f_t: torch.Tensor) -> torch.Tensor:
+        f_s_origin=f_s
+        f_s =FDA_source_to_target_np(f_s.detach().cpu().numpy(),f_t.detach().cpu().numpy(), L=0.25)  #fft变换后的feature
+        f_s = torch.tensor(f_s, dtype=torch.float32, requires_grad=True)
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        f_s=f_s.to(device)
+        f = torch.cat((f_s_origin,f_s,f_t), dim=0)     #和原来的一起输入
+        g = torch.cat((g_s,g_s,g_t), dim=0)
+        g = F.softmax(g, dim=1).detach()
+        h = self.grl(self.map(f, g))
+        d = self.domain_discriminator(h)
+        d_label = torch.cat((
+            torch.ones((g_s.size(0), 1)).to(g_s.device),
+            torch.ones((g_s.size(0), 1)).to(g_s.device),
+            torch.zeros((g_t.size(0), 1)).to(g_t.device),
+        ))
+        weight = 1.0 + torch.exp(-entropy(g))
+        batch_size = f.size(0)
+        weight = weight / torch.sum(weight) * batch_size
+        self.domain_discriminator_accuracy = binary_accuracy(d, d_label)
+        return self.bce(d, d_label, weight.view_as(d)),f_s
+
+
+class RandomizedMultiLinearMap(nn.Module):
+    """Random multi linear map
+
+    Given two inputs :math:`f` and :math:`g`, the definition is
+
+    .. math::
+        T_{\odot}(f,g) = \dfrac{1}{\sqrt{d}} (R_f f) \odot (R_g g),
+
+    where :math:`\odot` is element-wise product, :math:`R_f` and :math:`R_g` are random matrices
+    sampled only once and ﬁxed in training.
+
+    Args:
+        features_dim (int): dimension of input :math:`f`
+        num_classes (int): dimension of input :math:`g`
+        output_dim (int, optional): dimension of output tensor. Default: 1024
+
+    Shape:
+        - f: (minibatch, features_dim)
+        - g: (minibatch, num_classes)
+        - Outputs: (minibatch, output_dim)
+    """
+
+    def __init__(self, features_dim: int, num_classes: int, output_dim: Optional[int] = 1024):
+        super(RandomizedMultiLinearMap, self).__init__()
+        self.Rf = torch.randn(features_dim, output_dim)
+        self.Rg = torch.randn(num_classes, output_dim)
+        self.output_dim = output_dim
+
+    def forward(self, f: torch.Tensor, g: torch.Tensor) -> torch.Tensor:
+        f = torch.mm(f, self.Rf.to(f.device))
+        g = torch.mm(g, self.Rg.to(g.device))
+        output = torch.mul(f, g) / np.sqrt(float(self.output_dim))
+        return output
+
+
+class MultiLinearMap(nn.Module):
+    """Multi linear map
+
+    Shape:
+        - f: (minibatch, F)
+        - g: (minibatch, C)
+        - Outputs: (minibatch, F * C)
+    """
+
+    def __init__(self):
+        super(MultiLinearMap, self).__init__()
+
+    def forward(self, f: torch.Tensor, g: torch.Tensor) -> torch.Tensor:
+        batch_size = f.size(0)
+        output = torch.bmm(g.unsqueeze(2), f.unsqueeze(1))
+        return output.view(batch_size, -1)
+
+
+class ImageClassifier(ClassifierBase):
+    def __init__(self, backbone: nn.Module, num_classes: int, bottleneck_dim: Optional[int] = 256, **kwargs):
+        bottleneck = nn.Sequential(
+            # nn.AdaptiveAvgPool2d(output_size=(1, 1)),
+            # nn.Flatten(),
+            nn.Linear(backbone.out_features, bottleneck_dim),
+            nn.BatchNorm1d(bottleneck_dim),
+            nn.ReLU()
+        )
+
+        super(ImageClassifier, self).__init__(backbone, num_classes, bottleneck, bottleneck_dim, **kwargs)
